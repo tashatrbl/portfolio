3755f65987512b7cf23c12d5cce2256e6edcf8e0
dprtasha <carmelbarnabas@gmail.com>
2026-01-07 22:53:24 +0800

    reveal overlay fix

--- app/components/reveal.tsx
+++ app/components/reveal.tsx
@@ -2,48 +2,14 @@ import { motion } from "framer-motion";
 
 export const Reveal = ({ children, width = "100%" }) => {
   return (
-    <div
-      style={{
-        position: "relative",
-        width,
-        overflow: "hidden",
-      }}
+    <motion.div
+      style={{ width }}
+      initial={{ opacity: 0, y: 75 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: false, amount: 0.3 }}
+      transition={{ duration: 0.5, delay: 0.25 }}
     >
-      <motion.div
-        variants={{
-          hidden: { opacity: 0, y: 75 },
-          visible: { opacity: 1, y: 0 },
-        }}
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: false, amount: 0.3 }}
-        transition={{ duration: 0.5, delay: 0.25 }}
-        style={{
-          position: "relative",
-          zIndex: 10,
-        }}
-      >
-        {children}
-      </motion.div>
-
-      <motion.div
-        variants={{
-          hidden: { left: 0 },
-          visible: { left: "100%" },
-        }}
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: false, amount: 0.3 }}
-        transition={{ duration: 0.5, ease: "easeIn" }}
-        style={{
-          position: "absolute",
-          top: 0,
-          bottom: 0,
-          left: 0,
-          right: 0,
-          zIndex: 20,
-        }}
-      />
-    </div>
+      {children}
+    </motion.div>
   );
 };
